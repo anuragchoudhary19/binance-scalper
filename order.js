7b3f2180fbd42b22d205ce75db38fe3e60ca6533
anuragchoudhary1910 <anuragchoudhary1910@gmail.com>
2022-02-14 00:07:33 +0530

    first commit

--- order.js
+++ order.js
@@ -9,10 +9,10 @@ var binance = new Binance().options({
 exports.bookProfit = async (coin) => {
   const { symbol, positionSide, positionAmt } = coin;
   if (positionSide === 'LONG') {
-    console.log(await binance.futuresMarketSell(symbol, false, { positionSide: 'SHORT', closePosition: true }));
+    console.log(await binance.futuresMarketSell(symbol, false, { positionSide: 'SELL', closePosition: true }));
   }
   if (positionSide === 'SHORT') {
-    console.log(await binance.futuresMarketBuy(symbol, false, { positionSide: 'LONG', closePosition: true }));
+    console.log(await binance.futuresMarketBuy(symbol, false, { positionSide: 'BUY', closePosition: true }));
   }
 };
 
